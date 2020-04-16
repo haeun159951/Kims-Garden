40f11bb3ef72331c6d66001ac3fe0cc92d85694e
haeun159951 <43290846+haeun159951@users.noreply.github.com>
2020-04-16 17:43:18 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -7,29 +7,33 @@ function hideLists() {
   });
 }
 
-function showLists(productID) {
-  let gardenProduct = document.getElementById(productID);
+function showLists(productCard) {
+  let gardenProduct = document.getElementById(productCard);
   gardenProduct.style.display = 'block';
 }
 
-/*1. nav - tree*/
-document.getElementById('tree-link').addEventListener('click', function() {
-  hideLists();
-  showLists('tree-cards');
-});
+function setupMenuHandlers() {
+  document.getElementById('tree-link').addEventListener('click', function() {
+    hideLists();
+    showLists('tree-cards');
+  });
 
-/*2. nav - flower*/
-document.getElementById('flower-link').addEventListener('click', function() {
-  hideLists();
-  showLists('flower-cards');
-});
-/*3. nav - tool*/
-document.getElementById('tool-link').addEventListener('click', function() {
-  hideLists();
-  showLists('tool-cards');
-});
-/*4. nav - produce*/
-document.getElementById('produce-link').addEventListener('click', function() {
-  hideLists();
-  showLists('produce-cards');
-});
+  document.getElementById('flower-link').addEventListener('click', function() {
+    hideLists();
+    showLists('flower-cards');
+  });
+
+  document.getElementById('produce-link').addEventListener('click', function() {
+    hideLists();
+    showLists('produce-cards');
+  });
+
+  document.getElementById('tool-link').addEventListener('click', function() {
+    hideLists();
+    showLists('tool-cards');
+  });
+}
+
+window.onload = function() {
+  setupMenuHandlers();
+};
